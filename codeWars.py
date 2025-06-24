7d6a7a76caeba259727b3051f22b5f0bf1c69564
Zari <zjcmagnaye@gmail.com>
2025-06-23 20:42:03 -0700

    Remove String Spaces

--- codeWars.py
+++ codeWars.py
@@ -5,3 +5,8 @@ def even_or_odd(number):
 # Solution for Convert a Number to a String
 def number_to_string(num):
     return str(num)
+
+# Solution for Remove String Spaces
+def no_space(x):
+    final_string = "".join(x.split())
+    return final_string
